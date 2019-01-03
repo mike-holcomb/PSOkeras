b14a5115ea09e9f38e5be749b2e693f7f23eb1eb
mike-holcomb <mike-holcomb@users.noreply.github.com>
2019-01-03 17:24:26 -0600

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PSOkeras',
+      version='0.1.0',
+      description='Particle Swarm Optimization routine for Keras neural network models',
+      author='Mike Holcomb',
+      author_email='mjh170630@utdallas.edu',
+      url='https://github.com/mike-holcomb/PSOkeras',
+      packages=['psokeras'],
+      license='MIT',
+      classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7'],
+    )
